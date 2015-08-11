d750821e18ae418497d7a63219af0e1eed61039a
Harman A. <apu889@gmail.com>
2015-08-11 18:42:35 +0700

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,307 @@
+::-------------------::
+:: GitHub.com/apu889 ::
+::-------------------::
+
+TITLE S A D _ M A C H I N E
+MODE con lines=35 cols=152
+@ECHO OFF
+
+:: Cant find another way to delay-ing or ping-ing in milliseconds format, so here some of my dirty handworks.
+:loopdeloop
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 1.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 2.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 3.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 4.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 5.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 6.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 7.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 8.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 9.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 10.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 11.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 12.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 13.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 14.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 15.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 16.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 17.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 18.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 19.txt
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul 
+ping localhost -n 1 >nul
+cls
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+TYPE 5.txt
+
+goto loopdeloop
